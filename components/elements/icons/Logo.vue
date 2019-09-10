073b77c3ea006bffe69f0b85937af561be450983
Denver Elejorde <denver.elejorde@weave.ph>
2019-09-11 00:24:00 +0800

    Initial commit

---
+++ components/elements/icons/Logo.vue
@@ -0,0 +1,8 @@
+<template>
+    
+</template>
+<script>
+export default {
+    name: 'Logo'
+}
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,82 @@
+<template>
+  <v-app dark>
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+    >
+      <v-spacer />
+      <v-btn
+        icon
+        @click.stop="rightDrawer = !rightDrawer"
+      >
+        <v-icon>mdi-menu</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-navigation-drawer
+      v-model="rightDrawer"
+      :right="right"
+      temporary
+      fixed
+    >
+      <v-list>
+        <v-list-item v-for="item in items">
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-footer
+      :fixed="fixed"
+      app
+    >
+      <span>&copy; SOUTHSIDE STUDIOS PH 2019 all rights reserved</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Work',
+          to: '/work'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Clients',
+          to: '/clients'
+        }
+        ,
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Team',
+          to: '/team'
+        }
+        ,
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'About',
+          to: '/about'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Southside Studios PH'
+    }
+  }
+}
+</script>
